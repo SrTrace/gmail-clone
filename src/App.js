0c4cdc21a280257010c2fab2d2745b01101371ad
Sergey <levashov04sergey@gmail.com>
2021-05-17 23:06:32 +0300

    start project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+    return (
+        <div className="app">
+            <Header/>
+            <h2>Let's build gmail</h2>
+        </div>
+    );
+}
+
+export default App;
