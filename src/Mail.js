66a4e59fd73d251fb3ab9b3b835e2326a41d015c
SrTrace <levashov04sergey@gmail.com>
2021-05-19 23:26:21 +0300

    another block EmailLis

--- src/Mail.js
+++ src/Mail.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 import './Mail.css';
+import IconButton from "@material-ui/core/IconButton";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
-function Mail(props) {
+function Mail() {
+    const history = useHistory();
     return (
         <div className="mail">
+           <div className="mail__tools">
+               <div className="mail__toolsLeft">
+                    <IconButton
+                        onClick={()=>history.push("/")}
+                    >
+                    <ArrowBackIcon/>
+                    </IconButton>
 
+                   <IconButton>
+
+                   </IconButton>
+
+
+               </div>
+
+               <div className="mail__toolsRight">
+
+               </div>
+           </div>
+            <h1>What is up User?</h1>
         </div>
     );
 }
